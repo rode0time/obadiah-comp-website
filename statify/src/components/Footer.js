786d1bde49aa316381cf9256c62b5c430ef3bbbc
Obadia <captainkimmaasai@gmail.com>
2023-12-16 23:18:45 +0300

    some progres

--- statify/src/components/Footer.js
+++ statify/src/components/Footer.js
@@ -1,29 +1,50 @@
-import React from "react";
-import "../Styles/Footer.css";
+import React from 'react';
+import '../Styles/Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      <footer>
-        <div className="footer-content">
-          <img src="https://statify.co.ke/Images/IMG-20230305-WA0043.jpg" alt="Company Logo" className="footer-logo" />
-          <p>&copy; 2023 STATIFY.CO.KE | All rights reserved</p>
-          <p>Contact: info@statify.co.ke</p>
-          <div className="quick-links">
-            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-              Facebook
-            </a>
-            <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-              Twitter
-            </a>
-            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-              Instagram
-            </a>
-            <a href="mailto:info@statify.co.ke">Email</a>
+    <section className="footer">
+      <h4>About Us</h4>
+      <p>We are the best</p>
+      <div className="icons">
+        <i className="fa fa-facebook"></i>
+        <i className="fa fa-twitter"></i>
+        <i className="fa fa-instagram"></i>
+        <i className="fa fa-linkedin"></i>
+      </div>
+
+      {/* Your existing code */}
+      <footer className="footer">
+        <div className="logo">
+          <img src="https://statify.co.ke/Images/IMG-20230305-WA0043.jpg" alt="Company Logo" />
+        </div>
+        <div className="footer-links">
+          <div className="footer-section">
+            <h3>Contact Us</h3>
+            <ul>
+              <li><a href="mailto:obadiakimmaasai03@gmail.com">Email</a></li>
+              <li><a href="tel:+254728732301">Phone</a></li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h3>About Us</h3>
+            <ul>
+              <li><a href="/about">Our Story</a></li>
+              <li><a href="/team">Our Team</a></li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h3>Quick Links</h3>
+            <ul>
+              <li><a href="/products">Products</a></li>
+              <li><a href="/services">Services</a></li>
+            </ul>
           </div>
         </div>
       </footer>
-    </div>
+
+      <p>Powered by <i className="fa fa-heart-o"></i> Statify Company</p>
+    </section>
   );
 };
 
